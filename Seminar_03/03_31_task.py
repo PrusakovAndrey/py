387f24134b0b5a7bc9feb89c17175d316d1d19a2
Andrey <prusakov.andrey@gmail.com>
2022-01-31 22:19:24 +0300

    fixing

--- Seminar_03/03_31_task.py
+++ Seminar_03/03_31_task.py
@@ -11,23 +11,8 @@ def task31 (n):
             array.append(i) #3
             n = n / i
             i = 2
-            print (n)
         else: 
             i += 1
-            print (n)
     return (array)
 
 print (task31 (n))
-
-# def primfacs(n):
-#     i = 2
-#     primfac = []
-#     while i * i <= n:
-#         while n % i == 0:
-#             primfac.append(i)
-#             n = n / i
-#         i = i + 1
-#     if n > 1:
-#         primfac.append(n)
-#     return primfac
-# print (primfacs (n))
