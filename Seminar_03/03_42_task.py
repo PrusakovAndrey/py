6ab974cb0df81e0c22b25938dc6cf893f566dec0
Andrey <prusakov.andrey@gmail.com>
2022-02-10 11:50:05 +0300

    RLE is done

--- Seminar_03/03_42_task.py
+++ Seminar_03/03_42_task.py
@@ -1,6 +1,8 @@
 # Реализовать RLE алгоритм. реализовать модуль сжатия и восстановления данных.
 # входные и выходные данные хранятся в отдельных текстовых файлах
 
+
+# блок распаковки 
 path = '/Users/andreyprusakov/Desktop/GB projects/python/Seminar_03/42_coded.txt'
 def reading (path):
     f = open(path, 'r')
@@ -8,11 +10,9 @@ def reading (path):
     f.close()
     return data
 
-line = (reading(path))
-
 def splitting_to_tuple (a):
     a = a + ' '
-    n = '0,1,2,3,4,5,6,7,9'
+    n = '0,1,2,3,4,5,6,7,8,9'
     c = ''
     res = []
     r = ''
@@ -24,11 +24,37 @@ def splitting_to_tuple (a):
         if a[i] not in n: r = a[i]
         if a[i] in n: c += a[i]
     return res
-
 def printing (dt):
     str = ''
     for i in range (len(dt)):
         str += dt[i][0]*dt[i][1] 
     return str
 
-print (printing(splitting_to_tuple(line)))
+path_1 = '/Users/andreyprusakov/Desktop/GB projects/python/Seminar_03/42_decoded.txt'
+def writing (path,data):
+    f = open(path, 'w')
+    f.write(data)
+    f.close()
+
+writing(path_1, printing(splitting_to_tuple(reading(path))))
+
+# блок сжатия
+
+line = reading(path_1)
+print(line)
+
+
+def compressing (data):
+    data = data + ' '
+    count = 0
+    res = ''
+    for i in range (len(data)-1):
+        if data[i] == data[i+1]:
+            count +=1
+        else:
+            res += data[i]+str(count+1)
+            count = 0
+    return res
+
+
+print(compressing(line))
