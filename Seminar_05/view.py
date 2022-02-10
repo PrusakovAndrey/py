1db2cdc5ce6eb63ba90196c285439c2e502b6f1c
Andrey <prusakov.andrey@gmail.com>
2022-02-10 21:46:44 +0300

    uhsnc

--- Seminar_05/view.py
+++ Seminar_05/view.py
@@ -2,7 +2,7 @@ def view_data(data, title):
     print(f'{title} = {data}')
 
 def get_value():
-    return int(input('value = '))
+    return float(input('value = '))
 
 def get_function():
     return str(input('input on of: +, -, *, / -->> '))
